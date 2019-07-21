85249a3c961ab2a02975f4fa23b640ae72c13dc8
ulwan451 <52684583+ulwan451@users.noreply.github.com>
2019-07-21 07:00:25 +0700

    Update evaluasi.js

--- evaluasi.js
+++ evaluasi.js
@@ -31,42 +31,25 @@ document.write("Soal 2")
 document.write("<br>")
 document.write("<br>")
 
-// var banyakAngka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+////////////////////////////////
 
-// var genap = [];
-// var ganjil = [];
-// var i = 0;
+var banyakAngka=[20];
+var angkaGenap=[];
+var angkaGanjil=[];
 
-// 	for(i; i <banyakAngka.length; i++){
-// 		if (banyakAngka[i] % 2 == 0) {
-// 			genap.push(banyakAngka[i]);
-// 		}else{
-// 			ganjil.push(banyakAngka[i]);
-// 		}
-// 	}
-// 		document.write(genap + "<br>");
-// 		document.write(ganjil);
-
-var banyakAngka = [20];
-var genap = [];
-var ganjil = [];
-
-function klasifikasiAngka (){
-
-for (i = 2; i <= banyakAngka; i+=2){
-    document.write(i)
+function klasifikasiAngka(){
+    for(var i=1; i <= banyakAngka; i++){
+        if(i % 2 == 0){
+           angkaGenap.push(i)
+        }else{
+            angkaGanjil.push(i)
+        }
+    }
+    document.write(angkaGenap)
+    document.write('<br>')
+    document.write(angkaGanjil)
 }
-  genap.push(banyakAngka[i]);
-document.write("<br>")
-document.write("<br>")
-for (i = 1; i <= banyakAngka; i+=2){
-    document.write(i)
-}
-  ganjil.push(banyakAngka[i]);
-} 
-
 klasifikasiAngka()
-
 
  
 
